7762c03a64491e0833d5d714879def5287a3ca0c
Gregory Pereira <grpereir@redhat.com>
2025-08-18 15:47:27 -0700

    Fix node affinity patching for LWS wide-ep CI (#178)
    * patching value but not key out of affinity leaves statefulsets unschedulable
    
    Signed-off-by: greg pereira <grpereir@redhat.com>
    
    * leaving 2D-1P
    
    Signed-off-by: greg pereira <grpereir@redhat.com>
    
    ---------
    
    Signed-off-by: greg pereira <grpereir@redhat.com>

--- .github/scripts/e2e/wide-ep-transform.sh
+++ .github/scripts/e2e/wide-ep-transform.sh
@@ -27,7 +27,7 @@ NEW_MODEL_SED_ESCAPED="${NEW_MODEL_ORG}\/${NEW_MODEL_NAME}"
 patch() {
     # Transform ms-wide-ep/value{s.yaml to a scaled-down deployment
     # Need a small MoE model for this example: Qwen1.5-MoE-A2.7B-Chat.
-    # This example will be a 2 by 2 (1 replica, DP of 2 for decode, both using 2 GPUs)
+    # This example will be a 2D - 1P (DP of 2), 2 GPUs for prefill, and 1 GPU each for both Decodes
 
     yq e '.modelArtifacts.uri = "hf://'${NEW_MODEL}'"' -i ${FILE}
     yq e '.modelArtifacts.size = "30Gi"' -i ${FILE}
@@ -35,7 +35,7 @@ patch() {
 
     ## Decode specific changes
     ### Unbound the accelerator type from H200
-    yq e 'del(.decode.acceleratorTypes.labelValues)' -i ${FILE}
+    yq e 'del(.decode.acceleratorTypes)' -i ${FILE}
 
     ### Swap the model name in custom startup script
     decode_args=$(yq '.decode.containers[0].args[0]' ${FILE})
@@ -53,8 +53,8 @@ patch() {
     ### See above, example is a 2 by 2
     yq e '
     (.decode.containers[0].resources = {}) |
-    (.decode.containers[0].resources.limits = {"nvidia.com/gpu": 2}) |
-    (.decode.containers[0].resources.requests = {"nvidia.com/gpu": 2})
+    (.decode.containers[0].resources.limits = {"nvidia.com/gpu": 1}) |
+    (.decode.containers[0].resources.requests = {"nvidia.com/gpu": 1})
     ' -i ${FILE}
 
     ### Using model from HF rather than host storage, already discussed
@@ -72,7 +72,7 @@ patch() {
 
     ## Prefill specific changes
     ### Unbound the accelerator type from H200
-    yq e 'del(.prefill.acceleratorTypes.labelValues)' -i ${FILE}
+    yq e 'del(.prefill.acceleratorTypes)' -i ${FILE}
 
     ### Swap the model name in custom startup script
     prefill_args=$(yq '.prefill.containers[0].args[0]' ${FILE})
